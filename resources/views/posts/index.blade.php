c5a0bb3a7d8ad0e97d83372dab25b518feca2606
liyandro <johnzke08@gmail.com>
2024-04-03 17:49:55 +0800

    Added Index and show

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Activity 6</title>
+</head>
+
+<body>
+    <h1>My Activity 6</h1>
+    <ul>
+        <li><a href="/post/1">Post 1</a></li>
+        <li><a href="/post/2">Post 2</a></li>
+    </ul>
+</body>
+
+</html>
